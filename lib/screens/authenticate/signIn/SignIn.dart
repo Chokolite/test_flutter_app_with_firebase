4e65d823f8bf6bbaea850155e1596cc9e769fb17
Eduard Sivolotskiy <Eduard.Sivolotskiy@gmail.com>
2020-10-01 22:44:47 +0300

    added asset Google G logo and changed icon on sign in screen

--- lib/screens/authenticate/signIn/SignIn.dart
+++ lib/screens/authenticate/signIn/SignIn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:my_flutter_app_with_firebase/screens/authenticate/Register.dart';
 import 'package:my_flutter_app_with_firebase/screens/authenticate/signIn/SignIn_Provider.dart';
 import 'package:my_flutter_app_with_firebase/shared/Constants.dart';
@@ -7,6 +8,11 @@ import 'package:my_flutter_app_with_firebase/screens/authenticate/Email.dart';
 class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String googleLogo = "assets/svg/Google_Logo.svg";
+    final Widget googleSvgIcon = SvgPicture.asset(
+      googleLogo,
+      semanticsLabel: "Google G Logo",
+    );
     SignInProvider _auth = SignInProvider();
     return Scaffold(
       body: Container(
@@ -15,36 +21,60 @@ class SignIn extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Please Sign In or Register", style: textColor.copyWith(fontWeight: FontWeight.w500),),
-              SizedBox(height: 30,),
+              Text(
+                "Please Sign In or Register",
+                style: textColor.copyWith(fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 30,
+              ),
               Container(
                   color: buttonColor,
-                  child:
-                  FlatButton.icon(
+                  child: FlatButton.icon(
                       onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => Email()));},
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Email()));
+                      },
                       icon: Icon(Icons.email),
-                      label: Text("Email and password", style: textColor,))),
+                      label: Text(
+                        "Email and password",
+                        style: textColor,
+                      ))),
               SizedBox(
                 height: 20,
               ),
               Container(
                   color: buttonColor,
                   child: FlatButton.icon(
-                      onPressed: (){_auth.authWithGoogle();},
-                      icon: Icon(Icons.search),
-                      label: Text("Google", style: textColor,))),
+                      onPressed: () {
+                        _auth.authWithGoogle();
+                      },
+                      icon: SizedBox(
+                        height: 20,
+                        width: 20,
+                        child: googleSvgIcon,
+                      ),
+                      label: Text(
+                        "Google",
+                        style: textColor,
+                      ))),
               SizedBox(
                 height: 20,
               ),
               Container(
-                color: buttonColor,
+                  color: buttonColor,
                   child: FlatButton.icon(
                       onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => Register()));
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Register()));
                       },
                       icon: Icon(Icons.create),
-                      label: Text("Register", style: textColor,))),
+                      label: Text(
+                        "Register",
+                        style: textColor,
+                      ))),
             ],
           ),
         ),
